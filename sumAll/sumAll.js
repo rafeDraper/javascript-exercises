240cf420fba4b93ebf027d858dcce460b38e01c9
rafeDraper <rafedraper@gmail.com>
2019-02-16 22:22:17 +0100

    Updated answer RD

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+ const sumAll = (_one, _two) => {
+        if (typeof _one !== "number" || typeof _two !== "number" ) return "ERROR"
+        if (_one < 0 || _two < 0) return "ERROR"
 
-}
+        let arr = [_one, _two]
 
+        let max = Math.max(...arr);
+        let min = Math.min(...arr);
+    
+        let result = 0;
+        for (let i = min; i <= max; i++) {
+           result += i;
+        }
+    
+        return result;
+        
+    }
 module.exports = sumAll
