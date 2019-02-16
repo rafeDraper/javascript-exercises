935ef203af5f3e254782ad9b2edcd6d73c3fe41a
rafeDraper <rafedraper@gmail.com>
2019-02-16 22:05:56 +0100

    Updated RD sollution

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
+const leapYears = function(number) {
+    if (number === 0) return false;
+    if (number % 4 == 0) return true
+    if (number % 4 !== 0) return false
 
 }
-
+console.log(leapYears(2006))
 module.exports = leapYears
--- leapYears/leapYears.spec.js
+++ leapYears/leapYears.spec.js
@@ -4,19 +4,19 @@ describe('leapYears', function() {
   it('works with non century years', function() {
     expect(leapYears(1996)).toEqual(true);
   });
-  xit('works with non century years', function() {
+  ('works with non century years', function() {
     expect(leapYears(1997)).toEqual(false);
   });
-  xit('works with ridiculously futuristic non century years', function() {
+  ('works with ridiculously futuristic non century years', function() {
     expect(leapYears(34992)).toEqual(true);
   });
-  xit('works with century years', function() {
+  ('works with century years', function() {
     expect(leapYears(1900)).toEqual(false);
   });
-  xit('works with century years', function() {
+  ('works with century years', function() {
     expect(leapYears(1600)).toEqual(true);
   });
-  xit('works with century years', function() {
+  ('works with century years', function() {
     expect(leapYears(700)).toEqual(false);
   });
 });
