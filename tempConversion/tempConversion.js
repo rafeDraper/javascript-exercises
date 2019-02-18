233cb0b154ac86f29f8f48cc3965370c89666258
rafeDraper <rafedraper@gmail.com>
2019-02-18 10:00:59 +0100

    Updated RD answers

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(fValue) {
+  let result = 0;
+  //[°C] = ([°F] − 32) × ​5⁄9
+  result = (fValue - 32) * 5 / 9;
+  return Math.round(result * 10) / 10;
 }
 
-const ctof = function() {
+const ctof = function(cValue) {
+  let result = 0;
+  //[°F] = [°C] × ​9⁄5 + 32
+  result = cValue * 9/5 + 32;
+  return Math.round(result * 10) / 10;
 
 }
 
