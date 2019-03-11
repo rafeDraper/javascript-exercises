a0950cfcfcebe03490cd3ec3b017669a53f6f32e
rafeDraper <rafedraper@gmail.com>
2019-03-11 19:38:15 +0100

    My sollutions for the calculator exercises

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,27 +1,35 @@
-function add () {
-	
+function add (a,b) {
+	let sum = a+b;
+	return sum;
 }
 
-function subtract () {
-	
+function subtract (a,b) {
+	let subs = a-b;
+	return subs;	
 }
 
-function sum () {
-	
+function sum (input) {
+		const addition = input.reduce((a, b) => a + b, 0);
+		return addition; 	
 }
 
-function multiply () {
-	
+function multiply (input) {
+	return input.reduce((a, b) => a * b);
+}
+function power(a,b) {
+	return Math.pow(a,b)	
+};
+
+function factorial(input) {
+	if (input == 0) return 1;
+	let product = 1;
+	for (let i = input; i > 0; i--) {
+		product *= i;
+	}
+	return product;
 }
 
-function power() {
-	
-}
-
-function factorial() {
-	
-}
-
+console.log(factorial(5));
 module.exports = {
 	add,
 	subtract,
