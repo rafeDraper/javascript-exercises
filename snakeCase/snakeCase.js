868d1e34e6cb22f0c672de818f8166a011724cb0
rafeDraper <rafedraper@gmail.com>
2019-03-15 16:09:55 +0100

    solution to the snake Case

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,20 @@
-const snakeCase = function() {
+const snakeCase =  (input) =>  {
+    input = input.replace(/\.\./g, " ")
 
+    if (input.indexOf(" ") < 0) {
+        input = input.replace(/([A-Z])/g, " $1");
+        console.log(input)
+    }
+    return input
+        .trim()
+        .toLowerCase()
+        .replace(/[,\?\.]/g, "")
+        .replace(/\-/g, " ")
+        .split(" ")
+        .join("_");
 }
+
+
+console.log(snakeCase("HOLAHOLAHolaRafaComoEstas"))
 
 module.exports = snakeCase
