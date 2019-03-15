1a886bce6cc45c5351eedb0374793705a728cf9a
rafeDraper <rafedraper@gmail.com>
2019-03-15 14:03:19 +0100

    alternative sollution for the palindrome exercise

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function() {
+const palindromes = function(input) {
+    let remove = /[\W_]/g;
+    let lowCase = input.toLowerCase().replace(remove, '');
+    let reverse = lowCase.split('').reverse().join('');
 
+
+    return reverse === lowCase;
 }
 
 module.exports = palindromes
