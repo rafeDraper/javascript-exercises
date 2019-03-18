b34ed2e078e0465ed7c94a458bf7eb6c5606ad02
rafeDraper <rafedraper@gmail.com>
2019-03-18 09:36:38 +0100

    update

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function(input) {
-    
-}
+const fibonacci = (input) => {
+    if (input < 0) return "oh shit"
+    let a = 0;
+    let b = 1;
+    for (let i = 1; 1 < input; i++) {
+        const temp = b;
+        b = a + b;
+        a = temp;
+    }
+    return b;
+};
 
 module.exports = fibonacci
